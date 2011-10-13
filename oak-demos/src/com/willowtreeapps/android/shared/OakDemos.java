21134b09a2db7a0a348577ccd656073584486aa1
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-10-13 10:02:31 -0400

    Run ApiDemos as-is in the OAK Demos project.

--- oak-demos/src/com/willowtreeapps/android/shared/OakDemos.java
+++ oak-demos/src/com/willowtreeapps/android/shared/OakDemos.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.apis;
+package com.willowtreeapps.android.shared;
 
 import android.app.ListActivity;
 import android.content.Intent;
@@ -33,15 +33,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ApiDemos extends ListActivity {
+public class OakDemos extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         Intent intent = getIntent();
         String path = intent.getStringExtra("com.example.android.apis.Path");
-        
+
         if (path == null) {
             path = "";
         }
@@ -61,21 +61,22 @@ public class ApiDemos extends ListActivity {
         PackageManager pm = getPackageManager();
         List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
 
-        if (null == list)
+        if (null == list) {
             return myData;
+        }
 
         String[] prefixPath;
         String prefixWithSlash = prefix;
-        
+
         if (prefix.equals("")) {
             prefixPath = null;
         } else {
             prefixPath = prefix.split("/");
             prefixWithSlash = prefix + "/";
         }
-        
+
         int len = list.size();
-        
+
         Map<String, Boolean> entries = new HashMap<String, Boolean>();
 
         for (int i = 0; i < len; i++) {
@@ -84,9 +85,9 @@ public class ApiDemos extends ListActivity {
             String label = labelSeq != null
                     ? labelSeq.toString()
                     : info.activityInfo.name;
-            
+
             if (prefixWithSlash.length() == 0 || label.startsWith(prefixWithSlash)) {
-                
+
                 String[] labelPath = label.split("/");
 
                 String nextLabel = prefixPath == null ? labelPath[0] : labelPath[prefixPath.length];
@@ -97,7 +98,8 @@ public class ApiDemos extends ListActivity {
                             info.activityInfo.name));
                 } else {
                     if (entries.get(nextLabel) == null) {
-                        addItem(myData, nextLabel, browseIntent(prefix.equals("") ? nextLabel : prefix + "/" + nextLabel));
+                        addItem(myData, nextLabel, browseIntent(prefix.equals("") ? nextLabel
+                                : prefix + "/" + nextLabel));
                         entries.put(nextLabel, true);
                     }
                 }
@@ -105,28 +107,28 @@ public class ApiDemos extends ListActivity {
         }
 
         Collections.sort(myData, sDisplayNameComparator);
-        
+
         return myData;
     }
 
     private final static Comparator<Map<String, Object>> sDisplayNameComparator =
         new Comparator<Map<String, Object>>() {
-        private final Collator   collator = Collator.getInstance();
+            private final Collator collator = Collator.getInstance();
 
-        public int compare(Map<String, Object> map1, Map<String, Object> map2) {
-            return collator.compare(map1.get("title"), map2.get("title"));
-        }
-    };
+            public int compare(Map<String, Object> map1, Map<String, Object> map2) {
+                return collator.compare(map1.get("title"), map2.get("title"));
+            }
+        };
 
     protected Intent activityIntent(String pkg, String componentName) {
         Intent result = new Intent();
         result.setClassName(pkg, componentName);
         return result;
     }
-    
+
     protected Intent browseIntent(String path) {
         Intent result = new Intent();
-        result.setClass(this, ApiDemos.class);
+        result.setClass(this, OakDemos.class);
         result.putExtra("com.example.android.apis.Path", path);
         return result;
     }
@@ -141,7 +143,7 @@ public class ApiDemos extends ListActivity {
     @Override
     @SuppressWarnings("unchecked")
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        Map<String, Object> map = (Map<String, Object>)l.getItemAtPosition(position);
+        Map<String, Object> map = (Map<String, Object>) l.getItemAtPosition(position);
 
         Intent intent = (Intent) map.get("intent");
         startActivity(intent);
