12853444a61019abdd7eaee9acc92022f95f0f9f
Michael Lake <michael.lake@willowtreeapps.com>
2011-10-17 15:13:33 -0400

    Added Resized TextView to library with demo

---
+++ oak-demos/src/com/willowtreeapps/android/shared/ResizedTextViewActivity.java
@@ -0,0 +1,22 @@
+package com.willowtreeapps.android.shared;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+public class ResizedTextViewActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resized_textview_demo);
+
+
+
+    }
+}
---
+++ oak-library/src/main/java/com/willowtreeapps/android/shared/OAK.java
@@ -0,0 +1,11 @@
+package com.willowtreeapps.android.shared;
+
+/**
+ * User: mlake
+ * Date: 10/17/11
+ * Time: 12:19 PM
+ */
+public interface OAK {
+
+     static final String XMLNS = "http://willowtreeapps.com/oak/schema";
+}
