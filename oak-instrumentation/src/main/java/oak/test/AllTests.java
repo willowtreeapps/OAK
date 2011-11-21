38fb9e5da67a0c50879fa845d8c25f9de355a2bb
Michael Lake <michael.lake@willowtreeapps.com>
2011-11-21 16:30:25 -0500

    Adding copyright

--- oak-instrumentation/src/main/java/oak/test/AllTests.java
+++ oak-instrumentation/src/main/java/oak/test/AllTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2011. WillowTree Apps
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package oak.test;
 
 import android.test.suitebuilder.TestSuiteBuilder;
@@ -5,7 +21,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * User: mlake
+ * User: Michael Lake
  * Date: 9/20/11
  * Time: 10:45 AM
  */
