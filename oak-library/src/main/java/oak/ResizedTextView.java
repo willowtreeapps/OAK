6ca8a2b9805aff736e8368072146288184dc8b0e
Chris Thoma <chris.thoma@willowtreeapps.com>
2011-12-29 16:48:58 -0500

    Should have fixed the address issue

--- oak-library/src/main/java/oak/ResizedTextView.java
+++ oak-library/src/main/java/oak/ResizedTextView.java
@@ -16,10 +16,13 @@
 
 package oak;
 
+import android.R;
 import android.content.Context;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.text.Layout;
 import android.widget.TextView;
 
 
@@ -32,8 +35,9 @@ public class ResizedTextView extends TextViewWithFont {
     private String theText;
     private float lineSpacingMultiplier = 2.0f;
     private Layout textLayout;
+    private Layout textLayout2;
     private int counted;
-
+    private boolean notChecked;
 
 
     public ResizedTextView(Context context) {
@@ -62,6 +66,7 @@ public class ResizedTextView extends TextViewWithFont {
         setMaxLines(maxLines);
         theText = getText().toString();
         counted = 0;
+        notChecked = true;
     }
 
     @Override
@@ -79,8 +84,12 @@ public class ResizedTextView extends TextViewWithFont {
         this.maxBoundsWidth = this.getMeasuredWidth();
         this.setMeasuredDimension((int) this.maxBoundsWidth, this.getMeasuredHeight());
         if (counted == 0) {
+            textLayout = createWorkingLayout(theText);
+            textLayout2 = textLayout;
             setTextSize(findTextSize(1, 1));
             counted++;
+            Log.d("Final", new String() + getTextSize()  + "text size, " + textLayout.getLineCount()  + "line count" );
+
         }
         ellipsizeText();
     }
@@ -103,12 +112,19 @@ public class ResizedTextView extends TextViewWithFont {
                 this.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
                 newWidth = this.getMeasuredWidth();
             }
+
         }
         this.setTextSize(textSize);
         textLayout = createWorkingLayout(theText);
+        findAndSet(textLayout);
+
 
         if (textSize > minTextSize) {
-            if (numLines == 1 && ((textLayout.getLineCount() > 1) || (wouldEllipse()))) {
+            Log.d("HOWMANYLINES - ", new String() + textLayout2.getText().toString() + ", " +  textLayout2.getLineCount() );
+            if (numLines == 1 && ((textLayout2.getLineCount() > 1) || (wouldEllipse()))) {
+                return findTextSize(numLines, textSize - 4);
+            }
+            else if (numLines == 1 && notChecked && ((textLayout.getLineCount() > 1) || (wouldEllipse()))) {
                 return findTextSize(numLines, textSize - 4);
             }
             else {
@@ -176,5 +192,28 @@ public class ResizedTextView extends TextViewWithFont {
         return new StaticLayout(
                 workingText, getPaint(), getWidth() - getPaddingLeft() - getPaddingRight(), Layout.Alignment.ALIGN_NORMAL, lineSpacingMultiplier, 0.0f, false);
     }
+    
+    private void findAndSet(Layout lay) {
+        if (notChecked) {
+            int startLine = 0;
+            int endLine = 0;
+            CharSequence longestLine = lay.getText().subSequence(startLine, endLine);
+            for (int i = 1; i < lay.getLineCount() - 1; i++)
+            {
+                startLine = lay.getLineStart(i);
+                endLine = lay.getLineEnd(i);
+                CharSequence nLongestLine = lay.getText().subSequence(startLine, endLine);
+                if (nLongestLine.length() > longestLine.length()) {
+                    longestLine = nLongestLine;
+                    textLayout2 = createWorkingLayout(longestLine.toString());
+                    notChecked = false;
+                }
+            }
+
+        }
+    }
+
 
 }
+
+
