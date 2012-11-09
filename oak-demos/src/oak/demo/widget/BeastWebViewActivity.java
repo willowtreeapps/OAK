9a9ca6df673f6ec6f74ff9125fea78fee8db14a0
Derek Brameyer <derek.brameyer@gmail.com>
2012-11-09 15:25:26 -0500

    BeastWebView indeed...

--- oak-demos/src/oak/demo/widget/BeastWebViewActivity.java
+++ oak-demos/src/oak/demo/widget/BeastWebViewActivity.java
@@ -17,26 +17,23 @@ package oak.demo.widget;
 
 import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockFragment;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import oak.demo.OakDemoActivity;
 import oak.demo.R;
-import oak.widget.BeastViewPager;
+import oak.widget.BeastWebView;
 import roboguice.inject.InjectView;
 
-public class BeastViewPagerActivity extends OakDemoActivity {
+public class BeastWebViewActivity extends OakDemoActivity {
 
     @InjectView(R.id.view_pager) ViewPager mPager;
 
@@ -47,12 +44,14 @@ public class BeastViewPagerActivity extends OakDemoActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beast_view_pager_demo);
 
-        String[] fragmentTitles = new String[]{
-                "THIS",
-                "IS",
-                "A",
-                "TEST"
-        };
+        String demoText
+                = "Using BeastWebView, you can dispatch touch events to the parent View if you are on an edge of the WebView.  This is especially helpful for WebViews placed inside ViewPagers or other scrollable elements like a ListView, or for applications using the Sliding Menu/Side Navigation UI pattern.";
+
+        String[] fragmentTitles = new String[4];
+        fragmentTitles[0] = demoText;
+        fragmentTitles[1] = demoText;
+        fragmentTitles[2] = demoText;
+        fragmentTitles[3] = demoText;
 
         mAdapter = new TestFragmentAdapter(getSupportFragmentManager(), fragmentTitles);
         mPager.setAdapter(mAdapter);
@@ -90,23 +89,12 @@ public class BeastViewPagerActivity extends OakDemoActivity {
         public static TestFragment newInstance(String content) {
             TestFragment fragment = new TestFragment();
 
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < 20; i++) {
-                builder.append(content).append(" ");
-            }
-            builder.deleteCharAt(builder.length() - 1);
-            fragment.mContent = builder.toString();
+            fragment.mContent = content;
 
             return fragment;
         }
 
         private String mContent = "???";
-        private String[] mViewPagerContent = new String[]{
-                "Using BeastViewPager, you can dispatch touch events",
-                "to the parent View if you are on the leftmost or rightmost view.",
-                "This is especially helpful for ViewPagers inside ViewPagers,",
-                "or for applications using the Sliding Menu/Side Navigation UI pattern."
-        };
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -119,13 +107,12 @@ public class BeastViewPagerActivity extends OakDemoActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.test_fragment, null);
+            View view = inflater.inflate(R.layout.test_fragment_web_view, null);
 
-            BeastViewPager bvp = (BeastViewPager) view.findViewById(R.id.view_pager);
-            TestFragmentPagerAdapter tfpa = new TestFragmentPagerAdapter(mViewPagerContent);
-            bvp.setAdapter(tfpa);
+            WebView wv = (BeastWebView) view.findViewById(R.id.web_view);
+            wv.loadUrl("http://willowtreeapps.github.com/OAK/");
             TextView tv = (TextView) view.findViewById(R.id.text_view);
-            tv.setText(mContent + " " + mContent + " " + mContent + " " + mContent + " " + mContent + " " + mContent);
+            tv.setText(mContent);
 
             return view;
         }
@@ -134,49 +121,6 @@ public class BeastViewPagerActivity extends OakDemoActivity {
         public void onSaveInstanceState(Bundle outState) {
             super.onSaveInstanceState(outState);
             outState.putString(KEY_CONTENT, mContent);
-        }
-
-        private class TestFragmentPagerAdapter extends PagerAdapter {
-
-            private String[] testContent;
-
-            public TestFragmentPagerAdapter(String[] content) {
-                testContent = content;
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                TextView text = new TextView(getActivity());
-                text.setGravity(Gravity.CENTER);
-                text.setText(testContent[position]);
-                text.setTextSize(12 * getResources().getDisplayMetrics().density);
-                text.setPadding(20, 20, 20, 20);
-
-                LinearLayout layout = new LinearLayout(getActivity());
-                layout.setBackgroundColor(Color.GRAY);
-                layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT));
-                layout.setGravity(Gravity.CENTER);
-                layout.addView(text);
-                container.addView(layout);
-
-                return layout;
-            }
-
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object view) {
-                container.removeView((View) view);
-            }
-
-            @Override
-            public int getCount() {
-                return testContent.length;
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object o) {
-                return view.equals(o);
-            }
         }
     }
 }
--- oak-library/src/main/java/oak/widget/BeastWebView.java
+++ oak-library/src/main/java/oak/widget/BeastWebView.java
@@ -11,9 +11,10 @@ public class BeastWebView extends WebView {
     private float mCurrX = 0.0f;
     private float mCurrY = 0.0f;
     private int mTouchSlop;
-    private float oldX;
+    private boolean overScrollTop = false;
     private boolean overScrollLeft = false;
     private boolean overScrollRight = false;
+    private boolean overScrollBottom = false;
     private boolean isScrolling = false;
 
     public BeastWebView(Context context) {
@@ -54,17 +55,17 @@ public class BeastWebView extends WebView {
         // width of the vertical scrollbar
         int scrollBarWidth = getVerticalScrollbarWidth();
 
-        // width of the view depending of you set in the layout
+        // width of the view
         int viewWidth = computeHorizontalScrollExtent();
 
-        // width of the webpage depending of the zoom
+        // width of the webpage depending on the zoom
         int innerWidth = computeHorizontalScrollRange();
 
         // position of the left side of the horizontal scrollbar
         int scrollBarLeftPos = computeHorizontalScrollOffset();
 
         // position of the right side of the horizontal scrollbar, the width of scroll is the width of view minus the width of vertical scrollbar
-        int scrollBarRightPos = scrollBarLeftPos + viewWidth - scrollBarWidth;
+        int scrollBarRightPos = scrollBarLeftPos + viewWidth + scrollBarWidth;
 
         // if left pos of scroll bar is 0 left over scrolling is true
         if (scrollBarLeftPos == 0) {
@@ -80,32 +81,49 @@ public class BeastWebView extends WebView {
             overScrollRight = false;
         }
 
+        // height of the horizontal scrollbar
+        int scrollBarHeight = getHorizontalScrollbarHeight();
+
+        // height of the view
+        int viewHeight = computeVerticalScrollExtent();
+
+        // height of the webpage depending on the zoom
+        int innerHeight = computeVerticalScrollRange();
+
+        // position of the top side of the vertical scrollbar
+        int scrollBarTopPos = computeVerticalScrollOffset();
+
+        // position of the bottom of the vertical scrollbar, the height of scroll is the height of the view
+        int scrollBarBottomPos = scrollBarTopPos + viewHeight + scrollBarHeight;
+
+        // if top pos of scroll bar is 0 top over scrollling is true
+        if (scrollBarTopPos == 0) {
+            overScrollTop = true;
+        } else {
+            overScrollTop = false;
+        }
+
+        // if bottom pos of scroll bar is superior to webpage height: bottom over scrolling is true
+        if (scrollBarBottomPos >= innerHeight) {
+            overScrollBottom = true;
+        } else {
+            overScrollBottom = false;
+        }
+
         final int action = event.getAction();
-        if (action == MotionEvent.ACTION_DOWN) {
-            // if scrollbar is the most left or right
-            if (overScrollLeft || overScrollRight) {
-                isScrolling = false;
-            } else {
-                isScrolling = true;
-            }
-            oldX = event.getX();
-        } else if (action == MotionEvent.ACTION_MOVE) {
+        if (action == MotionEvent.ACTION_MOVE) {
             if (!isScrolling) {
-                if (event.getX() > oldX && overScrollLeft) {
+                if (((event.getX() - mCurrX) > mTouchSlop) && overScrollLeft) {
+                    // User moved finger to the right
                     getParent().requestDisallowInterceptTouchEvent(false);
-                }
-
-                if (event.getX() < oldX && overScrollRight) {
+                } else if (((mCurrX - event.getX()) > mTouchSlop) && overScrollRight) {
+                    // User moved finger to the left
                     getParent().requestDisallowInterceptTouchEvent(false);
-                }
-            }
-        } else if (action == MotionEvent.ACTION_UP) {
-            if (!isScrolling) {
-                if (event.getX() > oldX && overScrollLeft) {
+                } else if (((event.getY() - mCurrY) > mTouchSlop) && overScrollTop) {
+                    // User moved finger to the bottom
                     getParent().requestDisallowInterceptTouchEvent(false);
-                }
-
-                if (event.getX() < oldX && overScrollRight) {
+                } else if (((mCurrY - event.getY()) > mTouchSlop) && overScrollBottom) {
+                    // User moved finger to the top
                     getParent().requestDisallowInterceptTouchEvent(false);
                 }
             }
