51dea6b7a085d1d63500a4e9fae69ef8a18bc9ed
Derek Brameyer <derek.brameyer@gmail.com>
2013-02-19 11:43:13 -0500

    MaskedImageView stub demo activity...in progress.

---
+++ oak-demos/src/oak/demo/widget/MaskedImageViewActivity.java
@@ -0,0 +1,18 @@
+package oak.demo.widget;
+
+import android.os.Bundle;
+
+import oak.demo.OakDemoActivity;
+import oak.demo.R;
+
+/**
+ * User: Nate Date: 7/9/12 Time: 11:25 AM
+ */
+public class MaskedImageViewActivity extends OakDemoActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.masked_image_view_activity);
+    }
+}
