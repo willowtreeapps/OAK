afb5dccfc32e74f27545691cdfdd71cd4460bdda
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2013-10-16 17:10:12 -0400

    adding RatioLinearLayout

---
+++ oak-demos/src/oak/demo/widget/RatioLinearLayoutActivity.java
@@ -0,0 +1,18 @@
+package oak.demo.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import oak.demo.OakDemoActivity;
+import oak.demo.R;
+
+/**
+ * User: tylerromeo Date: 10/16/13 Time: 3:54 PM
+ */
+public class RatioLinearLayoutActivity extends OakDemoActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ratio_linear_layout_demo);
+    }
+}
