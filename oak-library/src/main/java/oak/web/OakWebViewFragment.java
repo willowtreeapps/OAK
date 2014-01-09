7cf3eb434f5487ddc87849c403b63b78fe28c529
Eric Richardson <eric.richardson97@gmail.com>
2014-01-09 17:54:51 -0500

    Added some OakWebViewFragment resources

--- oak-library/src/main/java/oak/web/OakWebViewFragment.java
+++ oak-library/src/main/java/oak/web/OakWebViewFragment.java
@@ -5,15 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import oak.R;
 
 /**
  * Created by ericrichardson on 1/9/14.
  */
 public class OakWebViewFragment extends Fragment {
 
-    private int layoutId;
     private String url;
+    public WebView webView;
 
     public static OakWebViewFragment getInstance(String url) {
         OakWebViewFragment fragment = new OakWebViewFragment();
@@ -26,12 +29,52 @@ public class OakWebViewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        url = getArguments().getString(WebViewActivity.EXTRA_URL);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        RelativeLayout root = new RelativeLayout(getActivity());
+        return inflater.inflate(R.layout.webview, container, false);
+    }
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        View back = view.findViewById(R.id.back);
+        View fwd = view.findViewById(R.id.forward);
+        View refresh = view.findViewById(R.id.refresh);
+        webView = (WebView) view.findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                }
+            }
+        });
+        fwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (webView.canGoForward()) {
+                    webView.goForward();
+                }
+            }
+        });
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl(webView.getUrl());
+            }
+        });
+
+        webView.loadUrl(url);
+
     }
 }
