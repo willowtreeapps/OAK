74d6509149fb728dbb7f46c09331c3c77e029845
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2014-03-10 13:55:55 -0400

    improved AspectRatioLayout, removed duplicate RatioLinearLayout

--- oak-demos/src/oak/demo/widget/AspectRatioLayoutActivity.java
+++ oak-demos/src/oak/demo/widget/AspectRatioLayoutActivity.java
@@ -1,6 +1,5 @@
 package oak.demo.widget;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import oak.demo.OakDemoActivity;
@@ -9,10 +8,10 @@ import oak.demo.R;
 /**
  * User: tylerromeo Date: 10/16/13 Time: 3:54 PM
  */
-public class RatioLinearLayoutActivity extends OakDemoActivity {
+public class AspectRatioLayoutActivity extends OakDemoActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ratio_linear_layout_demo);
+        setContentView(R.layout.aspect_ratio_layout_demo);
     }
 }
