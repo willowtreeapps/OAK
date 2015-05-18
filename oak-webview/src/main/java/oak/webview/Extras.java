c49226116613273b74546ad6c130bd85924796a1
Eric Richardson <eric.richardson97@gmail.com>
2015-05-18 16:43:07 -0400

    broke out webview into its own module.

---
+++ oak-webview/src/main/java/oak/webview/Extras.java
@@ -0,0 +1,18 @@
+package oak.webview;
+
+/**
+ * Created by WillowTree, Inc on 5/18/15.
+ */
+public class Extras {
+    public static final String EXTRA_URL = "oak_url";
+
+    public static final String EXTRA_LAYOUT = "oak_layout";
+    public static final String EXTRA_SHOW_CONTROLS = "oak_show_controls";
+    public static final String EXTRA_CONTROL_FADE = "oak_control_fade";
+    public static final String EXTRA_FADE_MIN = "oak_fade_min";
+    public static final String EXTRA_FADE_MAX = "oak_fade_max";
+    public static final String EXTRA_FADE_TIMEOUT = "oak_fade_timeout";
+
+    public static final String EXTRA_OPEN_IN_BROWSER = "oak_open_in_browser";
+    public static final String EXTRA_REFRESH_IN_MENU = "oak_refresh_in_menu";
+}
---
+++ oak-webview/src/main/java/oak/webview/WebViewActivity.java
@@ -0,0 +1,54 @@
+package oak.webview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+
+
+/**
+ * Activity that takes a url and displays it inside an OakWebViewFragment with Navigation
+ * Created by ericrichardson on 1/9/14.
+ */
+public class WebViewActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            if (!getIntent().hasExtra(Extras.EXTRA_URL)) {
+                throw new IllegalArgumentException("You must include a URL extra using OAK.EXTRA_URL in the bundle for this activity");
+            }
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            OakWebViewFragment.BundleBuilder builder = new OakWebViewFragment.BundleBuilder(getIntent().getExtras());
+            builder.openInBrowserEnabled(true);
+            fragmentTransaction.add(android.R.id.content, OakWebViewFragment.getInstance(builder.build()));
+            fragmentTransaction.commit();
+        }
+    }
+
+    public static Intent getIntent(Context context, Bundle bundle) {
+        Intent intent = new Intent(context, WebViewActivity.class);
+        intent.putExtras(bundle);
+        return intent;
+    }
+
+    public static void startWebActivity(Context context, String url) {
+        OakWebViewFragment.BundleBuilder builder = new OakWebViewFragment.BundleBuilder(url);
+        context.startActivity(getIntent(context, builder.build()));
+    }
+
+    public static void startWebActivity(Context context, Bundle bundle) {
+        context.startActivity(getIntent(context, bundle));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (((OakWebViewFragment) getSupportFragmentManager().findFragmentById(android.R.id.content)).canGoBack()) {
+            ((OakWebViewFragment) getSupportFragmentManager().findFragmentById(android.R.id.content)).back();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
